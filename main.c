73cdb965554181fee5d3bee5b013d0119d3c7852
Gustavo <tavinho_correa@hotmail.com>
2019-09-04 17:37:41 -0300

    Etapa 2

--- main.c
+++ main.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
-extern int yylex();
-extern int lc;
-extern int running;
-
-void initMe(void);
-int getLineNumber(void);
-int isRunning(void);
 
 int main(void)
 {
-  initMe();
-  int tok;
-  while(running){
-  tok=yylex();
-  if(!running){
-    break;
-  }
-  fprintf(stderr,"Found keyword %i\n",tok);
-  }
-  printf("The line number is: %d\n", getLineNumber());
-  return 0;
+    int tok;
+    initMe();
+    yyparse();
 }
