8e0bf7aacd2f59b58fc8c55b1a2f83fcc6391431
Gustavo <tavinho_correa@hotmail.com>
2019-11-30 11:04:55 -0300

    improved tac move

--- asm.s
+++ asm.s
@@ -6,13 +6,13 @@ main:
 .LFB0:
 	pushq	%rbp
 	movq	%rsp, %rbp
-## TAC_READ ##
-	movl	b(%rip), %eax
-	movl	%eax, %edi
-	movl	$0, %eax
-	call	read@PLT
-	movl	$0, %eax
+## TAC_MOVE ##
+	movl	c(%rip), %eax
+	movl	%eax, b(%rip)
+## TAC_MOVE ##
+	movl	$2, c(%rip)
 ## TAC_END_FUNC ##
+	movl	$0, %eax
 	popq	%rbp
 	ret
 .LFE0:
